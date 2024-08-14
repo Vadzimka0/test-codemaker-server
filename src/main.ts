7420dc629e6e3e5dd171a848a853c7067fc5fe12
Vadzim Dzianisik <wadik.nhl@gmail.com>
2024-08-14 03:27:17 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://209.38.42.190'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   });
 
   app.use(cookieParser());
